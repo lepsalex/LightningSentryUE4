34308d949d0af5f8055d29f574f27d2f7225bef6
Alex <lepsalex@gmail.com>
2018-12-14 17:46:39 -0500

    Initial basic setup

--- Source/LightningSentryUE4/LightningSentryUE4GameMode.cpp
+++ Source/LightningSentryUE4/LightningSentryUE4GameMode.cpp
@@ -1,15 +1,13 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 #include "LightningSentryUE4GameMode.h"
-#include "LightningSentryUE4Character.h"
+#include "Player/PlayerCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
-ALightningSentryUE4GameMode::ALightningSentryUE4GameMode()
-{
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+ALightningSentryUE4GameMode::ALightningSentryUE4GameMode() {
+    // set default pawn class to our Blueprinted character
+    static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/Behaviour/PlayerCharacter"));
+    if (PlayerPawnBPClass.Class != NULL) {
+        DefaultPawnClass = PlayerPawnBPClass.Class;
+    }
 }
