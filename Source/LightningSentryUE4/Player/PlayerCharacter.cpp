ca8ae714cdb92303c4062ee6f4c3e2af78f74b58
Alex <lepsalex@gmail.com>
2018-12-15 12:27:06 -0500

    WIP Camera angle

--- Source/LightningSentryUE4/Player/PlayerCharacter.cpp
+++ Source/LightningSentryUE4/Player/PlayerCharacter.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+// Copyright © Alexandru Lepsa 2019. All Rights Reserved.
 
 #include "PlayerCharacter.h"
 #include "HeadMountedDisplayFunctionLibrary.h"
@@ -34,13 +34,21 @@ APlayerCharacter::APlayerCharacter() {
     // Create a camera boom (pulls in towards the player if there is a collision)
     CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
     CameraBoom->SetupAttachment(RootComponent);
-    CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character
-    CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+    CameraBoom->TargetArmLength = BaseCameraDistance; // The camera follows at this distance behind the character (same as camera height)
+    CameraBoom->SetRelativeRotation(FRotator(0.0f, 53.1301024f, 0.0f)); // Isometric camera angle
+
+    // Do not rotate the arm based on the controller
+    CameraBoom->bUsePawnControlRotation = false;
+    CameraBoom->bInheritPitch = false;
+    CameraBoom->bInheritRoll = false;
+    CameraBoom->bInheritYaw = false;
 
     // Create a follow camera
     FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
     FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
     FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+    FollowCamera->SetRelativeLocation(FVector(0.0f, 0.0f, BaseCameraDistance));
+    FollowCamera->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
 
     // Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character)
     // are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
@@ -58,25 +66,9 @@ void APlayerCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerIn
     PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
     PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
 
-    // We have 2 versions of the rotation bindings to handle different kinds of devices differently
-    // "turn" handles devices that provide an absolute delta, such as a mouse.
-    // "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-    PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-    PlayerInputComponent->BindAxis("TurnRate", this, &APlayerCharacter::TurnAtRate);
-    PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-    PlayerInputComponent->BindAxis("LookUpRate", this, &APlayerCharacter::LookUpAtRate);
-
     // handle touch devices
     PlayerInputComponent->BindTouch(IE_Pressed, this, &APlayerCharacter::TouchStarted);
     PlayerInputComponent->BindTouch(IE_Released, this, &APlayerCharacter::TouchStopped);
-
-    // VR headset functionality
-    PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &APlayerCharacter::OnResetVR);
-}
-
-
-void APlayerCharacter::OnResetVR() {
-    UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
 }
 
 void APlayerCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location) {
@@ -87,18 +79,10 @@ void APlayerCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Locat
     StopJumping();
 }
 
-void APlayerCharacter::TurnAtRate(float Rate) {
-    // calculate delta for this frame from the rate information
-    AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-}
-
-void APlayerCharacter::LookUpAtRate(float Rate) {
-    // calculate delta for this frame from the rate information
-    AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
-}
-
 void APlayerCharacter::MoveForward(float Value) {
     if ((Controller != NULL) && (Value != 0.0f)) {
+        // TODO - Get offset of camera and calculate world forward when using analog input
+
         // find out which way is forward
         const FRotator Rotation = Controller->GetControlRotation();
         const FRotator YawRotation(0, Rotation.Yaw, 0);
@@ -111,6 +95,8 @@ void APlayerCharacter::MoveForward(float Value) {
 
 void APlayerCharacter::MoveRight(float Value) {
     if ( (Controller != NULL) && (Value != 0.0f) ) {
+        // TODO - Get offset of camera and calculate world forward when using analog input
+
         // find out which way is right
         const FRotator Rotation = Controller->GetControlRotation();
         const FRotator YawRotation(0, Rotation.Yaw, 0);
--- Source/LightningSentryUE4/Player/PlayerCharacter.h
+++ Source/LightningSentryUE4/Player/PlayerCharacter.h
@@ -1,4 +1,4 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+// Copyright © Alexandru Lepsa 2019. All Rights Reserved.
 
 #pragma once
 
@@ -6,67 +6,61 @@
 #include "GameFramework/Character.h"
 #include "PlayerCharacter.generated.h"
 
-UCLASS(config=Game)
-class APlayerCharacter : public ACharacter
-{
-	GENERATED_BODY()
+UCLASS(config = Game)
+class APlayerCharacter : public ACharacter {
+    GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
+    /** Camera boom positioning the camera behind the character */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    class USpringArmComponent * CameraBoom;
 
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
-public:
-	APlayerCharacter();
+    /** Follow camera */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    class UCameraComponent * FollowCamera;
 
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
+  public:
+    APlayerCharacter();
 
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
+    /** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+    float BaseTurnRate;
 
-protected:
+    /** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+    float BaseLookUpRate;
 
-	/** Resets HMD orientation in VR. */
-	void OnResetVR();
+  protected:
 
-	/** Called for forwards/backward input */
-	void MoveForward(float Value);
+    /** Called for forwards/backward input */
+    void MoveForward(float Value);
 
-	/** Called for side to side input */
-	void MoveRight(float Value);
+    /** Called for side to side input */
+    void MoveRight(float Value);
 
-	/** 
-	 * Called via input to turn at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void TurnAtRate(float Rate);
+    /** Handler for when a touch input begins. */
+    void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
 
-	/**
-	 * Called via input to turn look up/down at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
+    /** Handler for when a touch input stops. */
+    void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
 
-	/** Handler for when a touch input begins. */
-	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
+  protected:
+    // APawn interface
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    // End of APawn interface
 
-	/** Handler for when a touch input stops. */
-	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
+  public:
+    /** Returns CameraBoom subobject **/
+    FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 
-protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	// End of APawn interface
+    /** Returns FollowCamera subobject **/
+    FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
-public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+  private:
+
+    /** Used as boom length and camera vertical offset (multiplied by zoom) **/
+    float BaseCameraDistance = 1140;
+
+    /** Not currently used but would control camera zoom **/
+    float CameraZoom = 1;
 };
 
